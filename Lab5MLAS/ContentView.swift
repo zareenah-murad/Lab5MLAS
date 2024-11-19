dead80e1eb08606939334c03c3ff439450389be4
zareenah <zmurad@smu.edu>
2024-11-19 12:00:54 -0600

    Initial Commit

---
+++ Lab5MLAS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab5MLAS
+//
+//  Created by Zareenah Murad on 11/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab5MLAS/Lab5MLASApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab5MLASApp.swift
+//  Lab5MLAS
+//
+//  Created by Zareenah Murad on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab5MLASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
